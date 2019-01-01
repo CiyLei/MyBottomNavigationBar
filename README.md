8948155807a98375310d2398b52f4fd72760364d
CiyLei <903736662@qq.com>
2019-01-01 18:01:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# my_bottom_navigation_bar
+
+不知道为什么谷歌爸爸的`BottomNavigationBar`都不能自定义取消缩放，没办法拷出源码改（修改的地方有中文注释）。
+
+如果添加的`BottomNavigationBarItem`超过3个，请写死`type`
+
+```dart
+bottomNavigationBar: MyBottomNavigationBar(
+  type: MyBottomNavigationBarType.fixed,
+)
+```
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:my_bottom_navigation_bar/my_bottom_navigation_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyBottomNavigationBarDemo',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 1;
+  final _widgetOptions = [
+    Text('Index 0: Home'),
+    Text('Index 1: Business'),
+    Text('Index 2: School'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyBottomNavigationBarDemo'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: MyBottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('Business')),
+          BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('School')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+        type: MyBottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
